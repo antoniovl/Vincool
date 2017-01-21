330b908d00ab8eafb57d5861adb5781ac204d363
Luis Daniel <lalducin@nearsoft.com>
2017-01-20 20:30:37 -0600

    Create new view and diisplay calendar plugin

--- grails-app/controllers/vincool/SessionController.groovy
+++ grails-app/controllers/vincool/SessionController.groovy
@@ -4,4 +4,12 @@ class SessionController {
 
     static scaffold = Session
 
+    def showCalendar() {
+        def all = Session.all
+        def sessions = {
+            def hobbies = ["basketball", "photography"]
+        }
+        render(view: "calendar", model: [name: "Maricel"])
+    }
+
 }
