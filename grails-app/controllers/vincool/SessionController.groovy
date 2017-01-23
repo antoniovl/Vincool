4fb922f446f9506e4529139929d723e80020e153
Luis Daniel <ldanielalducin.developer@gmail.com>
2017-01-23 02:25:31 -0600

    Removed unused code from showCalendar in session controller

--- grails-app/controllers/vincool/SessionController.groovy
+++ grails-app/controllers/vincool/SessionController.groovy
@@ -5,11 +5,7 @@ class SessionController {
     static scaffold = Session
 
     def showCalendar() {
-        def all = Session.all
-        def sessions = {
-            def hobbies = ["basketball", "photography"]
-        }
-        render(view: "calendar", model: [name: "Maricel"])
+        render(view: "calendar")
     }
 
 }
