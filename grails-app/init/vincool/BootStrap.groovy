41b5231f8777ee96bcaed3893b00342bad8b3651
Luis Daniel <ldanielalducin.developer@gmail.com>
2017-01-24 23:31:18 -0600

    Modified initial data for testing

--- grails-app/init/vincool/BootStrap.groovy
+++ grails-app/init/vincool/BootStrap.groovy
@@ -13,20 +13,20 @@ class BootStrap {
         new Office(officeCode: Office.OfficeCode.CDMX, state: "Ciudad de México", city: "Ciudad de México", street: "San Luis Potosí",
                 number: 196, zipCode: "06700", phoneNumber:  "+52 (55) 6388-6650").save()
 
-        new Batch(edition: "Apprentice 2017 A").save()
-
-        new Lesson(topic: "Introductory Topics", description: "VCS and Python, Java, Javascript", syllabus: Batch.first()).save()
-        new Lesson(topic: "Data Structures and Big O Notation I", description: "Arrays and Lists", syllabus: Batch.first()).save()
-        new Lesson(topic: "Data Structures and Big O Notation II", description: "Sets and Hash Tables", syllabus: Batch.first()).save()
-
-        new Resource(url: "http://www.slides.com", description: "Slides about Arrays and Lists", lesson: Lesson.first()).save()
-        new Resource(url: "http://www.repo.com", description: "Repo with exercises about Arrays and Lists", lesson: Lesson.first()).save()
+        new Batch(edition: "Apprentice 2017 A", isActive: true).save()
 
         new Instructor(name: "Israel Gómez").save()
 
-        new Session(type: Session.Type.PRACTICAL,date: LocalDate.now(),time: LocalTime.now(), lesson: Lesson.findById(1), instructor: Instructor.first(), office: Office.first()).save()
-        new Session(type: Session.Type.PRACTICAL,date: LocalDate.parse("2017-01-26"),time: LocalTime.now(), lesson: Lesson.findById(2), instructor: Instructor.first(), office: Office.first()).save()
-        new Session(type: Session.Type.PRACTICAL,date: LocalDate.parse("2017-01-30"),time: LocalTime.now(), lesson: Lesson.findById(3), instructor: Instructor.first(), office: Office.first()).save()
+        new Lesson(topic: "Introductory Topics", description: "VCS and Python, Java, Javascript").save()
+        new Lesson(topic: "Data Structures and Big O Notation I", description: "Arrays and Lists").save()
+        new Lesson(topic: "Data Structures and Big O Notation II", description: "Sets and Hash Tables").save()
+
+        new Session(type: Session.Type.PRACTICAL, date: LocalDate.now(), time: LocalTime.now(), instructor: Instructor.first(), office: Office.first(), lesson: Lesson.get(1), batch: Batch.first()).save()
+        new Session(type: Session.Type.PRACTICAL, date: LocalDate.parse("2017-01-26"), time: LocalTime.now(), instructor: Instructor.first(), office: Office.first(), lesson: Lesson.get(2), batch: Batch.first()).save()
+        new Session(type: Session.Type.PRACTICAL, date: LocalDate.parse("2017-01-30"), time: LocalTime.now(), instructor: Instructor.first(), office: Office.first(), lesson: Lesson.get(3), batch: Batch.first()).save()
+
+        new Resource(url: "http://www.slides.com", description: "Slides about Arrays and Lists", lesson: Lesson.first()).save()
+        new Resource(url: "http://www.repo.com", description: "Repo with exercises about Arrays and Lists", lesson: Lesson.first()).save()
 
         new Enrollment(session: Session.findById(1), student: Student.findById(1), attendance: true).save()
         new Enrollment(session: Session.findById(1), student: Student.findById(2), attendance: false).save()
