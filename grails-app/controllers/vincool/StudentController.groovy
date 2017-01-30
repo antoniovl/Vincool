d92c183a8f4c4abc21ab3e81c8e4dab344d7d821
Luis Daniel <lalducin@nearsoft.com>
2017-01-30 12:23:03 -0600

    Added action in Student controller for enrolling

--- grails-app/controllers/vincool/StudentController.groovy
+++ grails-app/controllers/vincool/StudentController.groovy
@@ -7,6 +7,13 @@ class StudentController {
 
     static scaffold = Student
 
+    def springSecurityService
 
+    @Secured(['ROLE_STUDENT'])
+    def enroll(Long id) {
+        def student = (Student) springSecurityService.currentUser
+        new Enrollment(student: student, session: Session.findById(id), attendance: false).save(flush: true)
+        redirect(controller: "session", action: "calendar")
+    }
 
 }
