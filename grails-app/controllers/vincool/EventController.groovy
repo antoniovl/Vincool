e714b5fb6fb84f08984db64f6c0da4d640596ebe
Luis Daniel <lalducin@nearsoft.com>
2017-01-31 12:08:02 -0600

    Color for enrolled sessions in calendar

--- grails-app/controllers/vincool/EventController.groovy
+++ grails-app/controllers/vincool/EventController.groovy
@@ -14,21 +14,33 @@ class EventController {
     def springSecurityService
 
     def calendar() {
-        def events = Event.findAllByBatchInList(Batch.findAllByIsActive(true))
         def offices = Office.all
 
         def source = [:]
         offices.each { office ->
             source[office.officeCode] = [].withDefault{ [:] }
         }
+
+        def eventsEnrolledMap = [:]
+        if(isCurrentUserAStudent()) {
+            def eventsEnrolled = Enrollment.findAllByAttendee(springSecurityService.getCurrentUser())
+            eventsEnrolled.each { enrollment ->
+                eventsEnrolledMap[enrollment.event] = enrollment.attendee;
+            }
+        }
+
+        def color
+        def events = Event.findAllByBatchInList(Batch.findAllByIsActive(true))
         events.each{ event ->
+            color = event.eventCategory.color
+            if(eventsEnrolledMap[event] != null) { color = "gray" }
             source[event.office.officeCode]
                 .add([id: event.id,
                       title: event.eventCategory.subCategory,
                       start: event.date,
                       allDay: false,
-                      url: grailsLinkGenerator.link(controller: "event", action: "detail", id: event.id, absolute: true)])
-
+                      color: color,
+                      url: grailsLinkGenerator.link(controller: "calendar", action: "detail", id: event.id, absolute: true)])
         }
 
         [source: source as JSON, offices: offices]
@@ -38,22 +50,25 @@ class EventController {
 
         def event = Event.get(id)
         if (event == null) {
-            redirect(action: "calendar")
+            redirect(action: "index")
         }
 
         def isEnrolled = false
 
-        if(springSecurityService.isLoggedIn()) {
-            def roles = springSecurityService.getPrincipal().getAuthorities()
-
-            def rolesNames = roles.collect { it.getAuthority() }
-
-            if(rolesNames.contains("ROLE_STUDENT")) {
-                isEnrolled = Enrollment.findByAttendeeAndEvent(springSecurityService.getCurrentUser(), event) != null
-            }
+        if(isCurrentUserAStudent()) {
+            isEnrolled = Enrollment.findByAttendeeAndEvent(springSecurityService.getCurrentUser(), event) != null
         }
 
         [eventDetails: event, isEnrolled: isEnrolled]
+    }
+
+    private boolean isCurrentUserAStudent() {
+        if (springSecurityService.isLoggedIn()) {
+            def roles = springSecurityService.getPrincipal().getAuthorities()
+            def rolesNames = roles.collect { it.getAuthority() }
+            if (rolesNames.contains("ROLE_STUDENT")) { return true }
+        }
+        return false
     }
 
 }
