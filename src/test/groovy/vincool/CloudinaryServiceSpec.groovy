89c37587789f396ea3aab94e9c8097a8639ffa96
Israel Gomez <igomez@nearsoft.com>
2017-02-08 15:34:38 -0600

    Add missing files

---
+++ src/test/groovy/vincool/CloudinaryServiceSpec.groovy
@@ -0,0 +1,23 @@
+package vincool
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import vincool.cloudinary.CloudinaryService
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(CloudinaryService)
+class CloudinaryServiceSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
