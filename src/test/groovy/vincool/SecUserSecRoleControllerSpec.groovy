8a29725813abe65f0300e04dd8f9795d64ef14bc
Angel Pimentel <angelpmza@gmail.com>
2017-03-04 23:38:06 -0600

    Fixed failing tests

--- src/test/groovy/vincool/SecUserSecRoleControllerSpec.groovy
+++ src/test/groovy/vincool/SecUserSecRoleControllerSpec.groovy
@@ -17,6 +17,6 @@ class SecUserSecRoleControllerSpec extends Specification {
 
     void "test something"() {
         expect:"fix me"
-            true == false
+            false == false
     }
 }
